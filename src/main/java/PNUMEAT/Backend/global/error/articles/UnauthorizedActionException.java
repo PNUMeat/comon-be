3528e67c733c451b7eebf5b19dfe8688de7580e3
wvssm <neoramiregi@gmail.com>
2024-12-22 15:58:52 +0900

    refactor: UNAUTHORIZED 오타 수정

--- src/main/java/PNUMEAT/Backend/global/error/articles/UnauthorizedActionException.java
+++ src/main/java/PNUMEAT/Backend/global/error/articles/UnauthorizedActionException.java
@@ -6,7 +6,7 @@ import PNUMEAT.Backend.global.error.ErrorCode;
 
 public class UnauthorizedActionException extends ComonException {
     public UnauthorizedActionException() {
-        super(ErrorCode.UNAUTORIZED_ACTION);
+        super(ErrorCode.UNAUTHORIZED_ACTION);
     }
 
 }
