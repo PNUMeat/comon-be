fe516cc5367356fd4f98b51a33c562b37a3d16c5
jinwon1234 <ljw0626@naver.com>
2025-11-17 16:53:31 +0900

    chore: 사용하지 않는 @Slf4j 제거

--- src/main/java/site/codemonster/comon/global/security/filter/SseAccessFilter.java
+++ src/main/java/site/codemonster/comon/global/security/filter/SseAccessFilter.java
@@ -5,15 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import site.codemonster.comon.domain.auth.constant.AuthConstant;
 import site.codemonster.comon.domain.auth.entity.Member;
@@ -30,7 +26,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
-@Slf4j
 @RequiredArgsConstructor
 public class SseAccessFilter extends OncePerRequestFilter {
 
