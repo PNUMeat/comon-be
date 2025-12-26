f76a8405cb780b676cdb0d7bebf56ed8ef6b0975
pykido <animoto1@naver.com>
2025-12-27 01:20:55 +0900

    refactor : 관리자페이지 비밀번호 암호화

--- src/main/java/site/codemonster/comon/domain/adminAuth/service/AdminService.java
+++ src/main/java/site/codemonster/comon/domain/adminAuth/service/AdminService.java
@@ -1,6 +1,7 @@
 package site.codemonster.comon.domain.adminAuth.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.codemonster.comon.domain.adminAuth.dto.AdminLoginRequest;
@@ -13,13 +14,14 @@ import site.codemonster.comon.domain.adminAuth.repository.AdminMemberRepository;
 public class AdminService {
 
     private final AdminMemberRepository adminMemberRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public AdminMember authenticateAdmin(AdminLoginRequest request) {
         AdminMember adminMember = adminMemberRepository
                 .findByAdminId(request.getAdminId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 관리자 ID입니다."));
 
-        if (!request.getPassword().equals(adminMember.getPassword())) {
+        if (!passwordEncoder.matches(request.getPassword(), adminMember.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
