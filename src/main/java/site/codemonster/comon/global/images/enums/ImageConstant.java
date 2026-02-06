a32b2afcd83bdba13316087dec788fb341674c26
jinwon1234 <ljw0626@naver.com>
2026-02-06 11:15:04 +0900

    feat: default image url 추가

--- src/main/java/site/codemonster/comon/global/images/enums/ImageConstant.java
+++ src/main/java/site/codemonster/comon/global/images/enums/ImageConstant.java
@@ -1,8 +1,8 @@
 package site.codemonster.comon.global.images.enums;
 
 public enum ImageConstant {
-    DEFAULT_MEMBER_PROFILE("profile/default-image.png"),
-    DEFAULT_TEAM("team/default-image.png");
+    DEFAULT_MEMBER_PROFILE("https://codemonster-s3-bucket-v1.s3.ap-northeast-2.amazonaws.com/profile/default-image.png"),
+    DEFAULT_TEAM("https://codemonster-s3-bucket-v1.s3.ap-northeast-2.amazonaws.com/team/default-image.png");
 
     private final String objectKey;
 
