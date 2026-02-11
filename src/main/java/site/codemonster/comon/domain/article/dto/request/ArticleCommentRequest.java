b00ac00b867e5a641e751ea61f91b4eafc85e11a
pykido <animoto1@naver.com>
2026-02-11 17:52:31 +0900

    refactor : @NotEmptyHtml 어노테이션 적용

--- src/main/java/site/codemonster/comon/domain/article/dto/request/ArticleCommentRequest.java
+++ src/main/java/site/codemonster/comon/domain/article/dto/request/ArticleCommentRequest.java
@@ -1,10 +1,10 @@
 package site.codemonster.comon.domain.article.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import site.codemonster.comon.global.validation.annotation.NotEmptyHtml;
 
 public record ArticleCommentRequest(
-        @NotBlank(message = "댓글 내용은 필수요소입니다.")
+        @NotEmptyHtml(message = "댓글 내용은 필수요소입니다.")
         @Size(max = 300, message = "댓글 길이는 300자 이하입니다.")
         String description
 ) {}
